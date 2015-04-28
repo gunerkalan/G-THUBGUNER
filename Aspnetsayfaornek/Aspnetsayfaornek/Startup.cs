2a83facf05c5b1c15405c5ded93d6d713f06d430
Güner KALAN <gunerkalan@gmail.com>
2015-04-28 15:46:54 +0300

    debeneme
    c# web application

---
+++ Aspnetsayfaornek/Aspnetsayfaornek/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aspnetsayfaornek.Startup))]
+namespace Aspnetsayfaornek
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
